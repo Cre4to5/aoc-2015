fab9cf44a6a75f8371df70ca145b6175825042d3
KacperWojas <146821452+Cre4to5@users.noreply.github.com>
2024-08-17 21:36:14 +0200

    task5 part2

--- 5/part2.py
+++ 5/part2.py
@@ -4,22 +4,23 @@ with open("./5/input.txt", "r") as file:
     nices = []
     for line in lines:
         line = line.strip()
-        condition2 = False
-        print(line)
+        # print(line)
         seen = []
         seen2 = []
-        for i in range(1, len(line) - 2, 2):
-            seen2.append(f"{line[i]}{line[i + 1]}")
-        for i in range(0, len(line) - 1, 2):
-            seen.append(f"{line[i]}{line[i + 1]}")
-        has_repets = len(set(seen)) != len(seen) or len(set(seen2)) != len(seen2)
+        
+        for i in range(len(line) - 1):
+            pair = line[i:i + 2]
+            if pair in line[i + 2:]:
+                has_repets = True
+                break
 
+        condition2 = False
         for i in range(0, len(line) - 2):
             if line[i] == line[i + 2]:
-                condition2 =True
+                condition2 = True
         
         if condition2 and has_repets:
-            print("yes")
+            # print("yes")
             nices.append(line)
             nice_count += 1
     print(nices)
