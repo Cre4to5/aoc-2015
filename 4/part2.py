f8feaae9247e9a01da297933c7838bfadd6c220f
KacperWojas <146821452+Cre4to5@users.noreply.github.com>
2024-08-17 18:45:44 +0200

    part 2 of task 4 done

--- 4/part2.py
+++ 4/part2.py
@@ -1,31 +1,17 @@
-with open("./3/input.txt", "r") as file:
-    input = file.read()
-    x , y = 0, 0
-    x_r, y_r = 0, 0
-    visited = set()
-    for i, char in enumerate(input):
-        # print(char)
-        # print(x , y)
-        visited.add((x,y))
-        visited.add((x_r,y_r))
-        if i % 2 == 0:
-            match char:
-                case "^":
-                    y += 1
-                case ">":
-                    x += 1
-                case "v":
-                    y -= 1
-                case "<":
-                    x -= 1
-        else:
-            match char:
-                case "^":
-                    y_r += 1
-                case ">":
-                    x_r += 1
-                case "v":
-                    y_r -= 1
-                case "<":
-                    x_r -= 1
-    print(len(visited))
+import hashlib
+
+with open("./4/input.txt", "r") as file:
+    letters = file.read()
+    i = 1
+    while True:
+        input1 = letters + str(i)
+        # print(input1)
+        hashed = hashlib.md5(input1.encode('utf-8')).hexdigest()
+        # print(hashed)
+
+        if hashed.startswith("000000"):
+            potential = (hashed, i)
+            break
+        # print(f"{i/1_000}%")
+        i += 1
+    print(potential)
