2f4ace3eaf2d8bddf44fe1d0274701d76e2a0ba9
KacperWojas <146821452+Cre4to5@users.noreply.github.com>
2024-08-22 21:52:29 +0200

    humble start of task 9

--- 9/part1.py
+++ 9/part1.py
@@ -1,3 +1,23 @@
+import math
+
 with open("./9/input.txt", "r") as file:
     lines = file.readlines()
+
+    cities = set()
+    distances = {}
     
+    for line in lines:
+        line = line.split(" ")
+        cities.add(line[0])
+        cities.add(line[2])
+        distances[f"{line[0]}-{line[2]}"] = int(line[4])
+        distances[f"{line[2]}-{line[0]}"] = int(line[4])
+    
+
+    #TODO: better way for all permutations
+    for i in range(0,math.factorial(len(cities))):
+        cities_copy = cities
+        i = -1
+        while True:
+            i += 1
+            cities_copy
